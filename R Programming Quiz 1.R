a9467fd390bc343f601c4d0425c4bed94c6c7b48
lancer79 <lancerbrown79@gmail.com>
2020-03-03 08:06:48 -0800

    Quiz 1 day 2

--- R Programming Quiz 1.R
+++ R Programming Quiz 1.R
@@ -1,17 +1,17 @@
 install.packages("readr")
-x <- -4
+x <- 4
 class(x)
 x <- c(4, "a", TRUE)
 class(x)
 x <- c(1,3,5)
-y <- c(2,4,6)
-cbind(x,y)
+y <- c(3,2,10)
+rbind(x,y)
 x <- list(2, "a", "b", TRUE)
 x[[1]]
 x <- 1:4
 y <- 2:3
 x+y
-
+class(x+y)
 w1ds <- read.csv("hw1_data.csv")
 w1ds
 sum(is.na(w1ds$Ozone))
@@ -40,6 +40,7 @@ z <- w1ds %>%
   select(Ozone, Month) %>%
   filter(Month == 5)
 z
+max(z)
 x <- 4L
 class()
 class(x)
@@ -65,11 +66,31 @@ head(w1ds)
 tail(w1ds)
 good <- complete.cases(w1ds)
 good
-which(!complete.cases(w1ds))
-na_vec <- which(!complete.cases(w1ds))
+which(complete.cases(w1ds))
+na_vec <- which(complete.cases(w1ds$Ozone))
 na_vec
-w1ds_no_NA <- w1ds[-na_vec,]
+w1ds_no_NA <- w1ds[na_vec,]
 w1ds_no_NA
 w1ds_NAomit <- na.omit(w1ds)
 w1ds_NAomit
 mean(w1ds_NAomit$Ozone)
+
+w1ds[1]
+w1ds[[1]]
+w1ds[c(1,3), "Ozone"]
+
+x <- list(2, "a", "b", TRUE)
+y <- x[[1]]
+class(y)
+
+x <- c(17, 14, 4, 5, 13, 12, 10) 
+x[x>10]<-4
+x[x>=11]<-4
+x
+max(w1ds$Ozone, na.rm = TRUE)
+x <- c(1,3, 5)
+y <- c(3, 2, 10)
+cbind(x,y)
+x <- list(2, "a", "b", TRUE)
+x[[2]]
+class(x[[2]])
