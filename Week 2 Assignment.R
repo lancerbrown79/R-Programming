40c22c90d107090668fd2d9a02643a0ea4d761ee
lancerbrown79 <lancerbrown79@gmail.com>
2020-03-05 21:34:03 -0800

    Assignment 1 pollutantmean

--- Week 2 Assignment.R
+++ Week 2 Assignment.R
@@ -19,3 +19,20 @@ head(data1_2)
 tail(data1_2)
 head(data1_2)
 test
+filelist <- list.files("C:/Users/battl/Dropbox/Lancer/R Programming/Coursera/specdata", pattern = "*.csv", full.names=TRUE)
+values <- numeric()
+data <- read.csv(filelist[1])
+values <- c(values, data$sulfate)
+values
+mean(values, na.rm = TRUE)             
+pollutantmean <- function(path, pollutant, id = 1:332) {
+  filelist <- list.files("C:/Users/battl/Dropbox/Lancer/R Programming/Coursera/specdata", pattern = "*.csv", full.names=TRUE)
+  values <- numeric()
+  for(i in id) {
+    data <- read.csv(filelist[i])
+    values <- c(values, data[[pollutant]])
+  }
+  mean(values, na.rm = TRUE)
+}
+
+pollutantmean("C:/Users/battl/Dropbox/Lancer/R Programming/Coursera/specdata", pollutant = "sulfate")
