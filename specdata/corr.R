c8a118a1b93c62ec64cdd111f853a2d550183fd9
lancer79 <lancerbrown79@gmail.com>
2020-03-06 15:44:42 -0800

    corr.r update

--- specdata/corr.R
+++ specdata/corr.R
@@ -15,3 +15,30 @@ corr <- function(directory, threshold = 0) {
   }
   
 }
+
+corr <- function(directory, threshold=0) {
+  
+  # Use complete to get cases and numer of complete records
+  cases_ncomplete<-complete(directory,1:332)
+  
+  # Select only cases within the threshold
+  selected_cases<-subset(cases_ncomplete, nobs>threshold, select = id)
+  selected_cases<-as.vector(selected_cases$id)
+  
+  # Initialize the vector
+  c<-c()
+  
+  # Correlation between sulfate and nitrate for the selected cases
+  for (i in selected_cases) {      
+    id_fs<-formatC(i,width=3, flag="0")   
+    tmp<-read.csv(paste(directory,"/",as.character(id_fs),".csv", sep=''))
+    tmp <- tmp[complete.cases(tmp),]
+    cor<-cor(tmp$sulfate,tmp$nitrate)
+    c<-c(c,cor)  
+  }  
+  
+  # End
+  return(c)
+}
+
+corr("specdata", 3)
