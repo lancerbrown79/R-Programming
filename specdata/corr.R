d238f326aa64aa63f5a9a7c7bf5c72d85749d3ac
lancer79 <lancerbrown79@gmail.com>
2020-03-09 09:43:49 -0700

    Corr.r

--- specdata/corr.R
+++ specdata/corr.R
@@ -1,20 +1,3 @@
-corr <- function(directory, threshold = 0) {
-  filelist <- list.files("/Users/lancer/Documents/R Programming/CourseEra/specdata/", pattern = "*.csv", full.names=TRUE)
-  dat_csv <- ldply(filelist, read_csv)
-  dat_csvcc <- complete.cases(dat_csv)
-  cc1 <- dat_csv[dat_csvcc, ]
-  z <- data.frame()
-  for (i in 1:332) {
-    rowcount <- nrow(filter(cc1, ID==i))
-    if(rowcount < threshold) {
-      print(as.numeric(c(0)))
-    } else {
-      thresh <- rbind(z, filter(cc1, ID==i))
-      print(cor(thresh$nitrate, thresh$sulfate))
-    }
-  }
-  
-}
 
 corr <- function(directory, threshold=0) {
   
@@ -40,10 +23,3 @@ corr <- function(directory, threshold=0) {
   # End
   return(c)
 }
-
-corr("specdata", 3)
-getwd()
-setwd()
-id_fs<-formatC(1,width=3, flag="0")  
-id_fs
-tmp<-read.csv(paste("/Users/lancer/Documents/R Programming/CourseEra/specdata","/",as.character(id_fs),".csv", sep=""))
