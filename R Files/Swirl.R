fbde479d65ec8a6419f3d6e73f14f213d2dff443
lancer79 <lancerbrown79@gmail.com>
2020-03-17 10:02:37 -0700

    looking at data, basic plots

--- R Files/Swirl.R
+++ R Files/Swirl.R
@@ -300,13 +300,72 @@ tapply(flags$population, flags$landmass, summary)
 lancerbrown79@gmail.com
 avlaE5zBaljt0xad
 0
-
-
-
-
-
-
-
-
-
-
+12
+ls()
+class(plants)
+dim(plants)
+nrow(plants)
+ncol(plants)
+object.size(plants)
+names(plants)
+head(plants)
+head(plants, 10)
+tail(plants, 15)
+summary(plants)
+table(plants$Active_Growth_Period)
+str(plants)
+?sample()
+?sample
+sample(1:6, 4, replace = TRUE)
+sample(1:20, 10)
+?LETTERS
+sample(LETTERS)
+LETTERS
+flips <- sample(c(0,1),100, replace = TRUE, prob = c(0.3, 0.7))
+flips
+sum(flips)
+?rbinom
+rbinom(1, size = 100, prob = 0.7)
+flips2 <- rbinom(100, size = 1, prob = 0.7)
+flips2
+sum(flips2)
+?rnorm
+rnom(10)
+rnorm(10)
+rnorm(10, 100, 25)
+?rpois
+rpois(5, 10)
+my_pois <- replicate(100, rpois(5, 10))
+my_pois
+cm <- colMeans(my_pois)
+hist(cm)
+data(cars)
+?cars
+head(cars)
+plot(cars)
+?plot
+plot(x = cars$speed, y = cars$dist)
+plot(x = cars$dist, y = cars$speed)
+plot(x = cars$speed, y = cars$dist, xlab = "Speed")
+plot(x = cars$speed, y = cars$dist, ylab = "Stopping Distance")
+plot(x = cars$speed, y = cars$dist, xlab = "Speed", ylab = "Stopping Distance")
+plot(cars, main = "My Plot")
+plot(cars, sub = "My Plot Subtitle")
+plot(cars, col = 2)
+plot(cars, xlim = c(10, 15))
+plot(cars, pch = 2)
+data(mtcars)
+play()
+str(mtcars)
+play
+play()
+next()
+nxt()
+?boxplot
+boxplot(mtcars, mpg ~ cyl)
+boxplot(formula = mpg ~ cyl, data = mtcars)
+hist(mtcars$mpg)
+1
+lancerbrown79@gmail.com
+Iv9CHLEgo5vnlHZz
+2
